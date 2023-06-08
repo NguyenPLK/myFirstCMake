28414e34c67c6e8163b32594ba79ac39a277b216
NguyenPLK <mrnguyen1601@gmail.com>
2023-06-08 22:02:34 +0700

    my first commit

---
+++ usingCMake/CMakeFiles/myCMake.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/myCMake.dir/myFirstCmake.cpp.o"
+  "CMakeFiles/myCMake.dir/myFirstCmake.cpp.o.d"
+  "myCMake"
+  "myCMake.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/myCMake.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
